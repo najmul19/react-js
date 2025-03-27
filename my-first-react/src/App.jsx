3d0b205b784a8c9e6326c42a33593e553c1a2bed
najmul <mdnajmulislam10992@gmail.com>
2025-03-27 15:45:53 +0600

    Explore JSX and props basic and some styling

--- my-first-react/src/App.jsx
+++ my-first-react/src/App.jsx
@@ -1,36 +1,54 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      
       <h1>Vite + React</h1>
       <Persion></Persion>
-      <Persion></Persion>
-      <Persion></Persion>
-      <Persion></Persion>
-      
+      <St></St>
+      <Dev></Dev>
+      <Device name="Laptop" price="55"></Device>
+      <Device name="Mobile" price="19"></Device>
     </>
-  )
+  );
 }
 
-function Persion(){
+function Persion() {
   const age = 23;
-  return <h3>I am a person with age: {age}</h3>
+  return <h3>I am a person with age: {age}</h3>;
 }
-function St(){
+function St() {
   return (
-    <div>
+    <div className="student">
       <h3>Ok</h3>
       <p>Name</p>
       <p>Age</p>
     </div>
-  )
+  );
 }
 
-export default App
+function Device(props){
+  console.log(props)
+  return <h2>This device: {props.name} price: {props.price}</h2>
+}
+function Dev() {
+  const devStyle = {
+    border: "2px solid purple",
+    padding: "20px",
+    margin: "20px",
+    borderRadius: "10px",
+  };
+  return (
+    <div style={devStyle}>
+      <h5>Dev Dev</h5>
+      <p>Hello</p>
+    </div>
+  );
+}
+
+export default App;
