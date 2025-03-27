f94c94aab1792ae49961647d1ee4b9bed07836d1
najmul <mdnajmulislam10992@gmail.com>
2025-03-27 16:41:31 +0600

    six way to do conditional rendering if,ternary &&/||

--- my-first-react/src/Todo.jsx
+++ my-first-react/src/Todo.jsx
@@ -15,8 +15,31 @@
 // }
 
 //conditional rendaring option-2
+// export default function Todo({task, isDone}){
+//     if(isDone){
+//         return <li>FInished: {task}</li>
+//     } return <li>Work on: {task}</li>
+// }
+
+
+//conditional rendaring option-3, ternary operator
+// export default function Todo({task, isDone}){
+//     return (
+//         <li>{isDone? "FInished":"Work On"}: {task}</li>
+//     )
+// }
+
+
+// //conditional rendaring option-4, &&
+// export default function Todo({task, isDone}){
+//     return (
+//         <li>{task} {isDone && "Done"}</li>
+//     )
+// }
+
+//conditional rendaring option-5, ||
 export default function Todo({task, isDone}){
-    if(isDone){
-        return <li>FInished: {task}</li>
-    } return <li>Work on: {task}</li>
+    return (
+        <li>{task} {isDone || "Do it"}</li>
+    )
 }
