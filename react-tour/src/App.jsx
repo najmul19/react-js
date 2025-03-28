2d128f48f9ba72611b4979aa468ae7893be8884e
najmul <mdnajmulislam10992@gmail.com>
2025-03-29 02:06:10 +0600

    diplay countries in a simple way and folder structured

---
+++ react-tour/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Countries from './components/Countries/Countries'
+
+function App() {
+ 
+  return (
+    <>
+      <Countries></Countries>
+      <h1>Vite + React</h1>
+      
+    </>
+  )
+}
+
+export default App
