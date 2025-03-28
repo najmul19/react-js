81162506c7f8502a06d9ebfd3da57f563e04a867
najmul <mdnajmulislam10992@gmail.com>
2025-03-29 03:31:45 +0600

    Toggle state based on click and conditional rending

--- react-tour/src/components/Countries/Countries.jsx
+++ react-tour/src/components/Countries/Countries.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import Country from "../Country/Country";
-
+import './Countries.css'
 
 const Countries = () => {
     const [countries, setCountries] =useState( [])
@@ -10,13 +10,15 @@ const Countries = () => {
         .then(data=>setCountries(data));
     } ,[])
     return (
-        <div>
+        <div >
              <h3>Countries:{countries.length} </h3>
-             {
+            <div className="country-container">
+            {
                 countries.map(country=> <Country
                     key={country.cca3}
                     country={country}></Country>)
              }
+            </div>
          </div>
     );
 };
