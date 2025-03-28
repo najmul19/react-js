e344caf97efe53b14489e97ca0955606e0fc4810
najmul <mdnajmulislam10992@gmail.com>
2025-03-29 05:08:28 +0600

    handleVisitedCountry(country) and Updating state in arrays and pass parameter

--- react-tour/src/components/Countries/Countries.jsx
+++ react-tour/src/components/Countries/Countries.jsx
@@ -1,26 +1,42 @@
 import { useState } from "react";
 import Country from "../Country/Country";
-import './Countries.css'
+import "./Countries.css";
 
 const Countries = () => {
-    const [countries, setCountries] =useState( [])
-    useState(()=>{
-        fetch('https://restcountries.com/v3.1/all')
-        .then(res=> res.json())
-        .then(data=>setCountries(data));
-    } ,[])
-    return (
-        <div >
-             <h3>Countries:{countries.length} </h3>
-            <div className="country-container">
+  const [countries, setCountries] = useState([]);
+  const [visitedCountries, setVisitedCountries]= useState([])
+  useState(() => {
+    fetch("https://restcountries.com/v3.1/all")
+      .then((res) => res.json())
+      .then((data) => setCountries(data));
+  }, []);
+  const handleVisitedCountry=(country)=>{
+    console.log('add this to your visited country')
+    // console.log(country)
+    const newVisitedCountries = [...visitedCountries, country];
+    setVisitedCountries(newVisitedCountries);
+  }
+  return (
+    <div>
+      <h3>Countries:{countries.length} </h3>
+      <div>
+        <h5>Visited Countries: {visitedCountries.length} </h5>
+        <ul>
             {
-                countries.map(country=> <Country
-                    key={country.cca3}
-                    country={country}></Country>)
-             }
-            </div>
-         </div>
-    );
+                visitedCountries.map(country=> <li key={country.cca3}>{country.name.common}</li> )
+            }
+        </ul>
+      </div>
+
+      <div className="country-container">
+        {countries.map((country) => (
+          <Country key={country.cca3} 
+          handleVisitedCountry={handleVisitedCountry}
+          country={country}></Country>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Countries;
