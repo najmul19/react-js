4e2ac48a211d2be67d39d0e0397a44fd8b8cdeaf
najmul <mdnajmulislam10992@gmail.com>
2025-03-30 17:06:33 +0600

    ES6 Modules, import default, relative path

---
+++ es6-modules-overview/src/Utils/Calculate.js
@@ -0,0 +1,14 @@
+const add = (f,s)=>{
+    return f+s;
+}
+const mul=(f,s)=>{return f*s;}
+
+const divideTheFirstNumberByTheSecondNumber=(f,s)=>{
+    return f/s;
+}
+
+export {add,mul, divideTheFirstNumberByTheSecondNumber}
+
+// export default add
+// const calculator={add:add}
+// export default calculator;
---
+++ es6-modules-overview/src/components/Sunglass/Sunglass.jsx
@@ -0,0 +1,20 @@
+import './Sunglass.css'
+import Watch from '../Watch/Watch'
+import { add, mul, divideTheFirstNumberByTheSecondNumber as divide } from '../../Utils/Calculate';
+import { useEffect } from 'react';
+// import add from '../../Utils/Calculate';
+const Sunglass = () => {
+    const f = 39;
+    const s=93;
+    const sum = add(f,s); 
+    const multi=mul(f,s)
+    const vag = divide(f,s);
+    useEffect()
+    return (
+        <div>
+            <Watch></Watch>
+        </div>
+    );
+};
+
+export default Sunglass;
