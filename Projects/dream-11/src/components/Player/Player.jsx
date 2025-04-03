33a7c5d3184cf4ad9f287e0868322023faee8fa1
najmul <mdnajmulislam10992@gmail.com>
2025-04-03 23:17:43 +0600

    complete to show data and design the card players component and player card

--- Projects/dream-11/src/components/Player/Player.jsx
+++ Projects/dream-11/src/components/Player/Player.jsx
@@ -1,15 +1,44 @@
+import flag from "../../assets/images/red-flag.png"
+import person from "../../assets/images/user.png"
 
-
-const Player = ({player}) => {
-    const {name} = player;
-    console.log(player)
-    return (
-        <div>
-            <div>
-                
+const Player = ({ player }) => {
+  const { img, name, country, batting_style, price,category } = player;
+  console.log(player);
+  return (
+    <div className="grid grid-cols-3 w-full">
+      <div className="shadow-xl w-[400px] h-[550px] bg-gray-200 rounded-md ">
+        <div className="flex flex-col  p-2 items-center justify-center">
+          <div className="py-5">
+            <img className="h-[350px] w-[350px] rounded-md" src={img} alt="" />
+          </div>
+          <div className="w-[350px] flex flex-col ">
+            <div className="flex gap-2 items-center">
+              <img className="h-[30px] w-[30px] " src={person} alt="" />
+              <h2 className="font-bold text-black">{name}</h2>
             </div>
+            <div className="flex justify-between items-center pt-2">
+              <div className="flex gap-2">
+              <img className="h-[30px] w-[30px]" src={flag} alt="" />
+              <h2 className=" text-gray-500">{country}</h2>
+              </div>
+              <p className="px-2 py-1 rounded-md bg-gray-300">{category}</p>
+            </div>
+            
+            <hr className=" my-2 border-t-2 border-gray-400" />
+
+            <div className="flex justify-between items-center pb-1">
+              <h2 className="font-bold text-black">{batting_style}</h2>
+              <p className=" text-gray-500">{batting_style}</p>
+            </div>
+            <div className="flex justify-between items-center">
+              <h2 className="font-bold text-black " >Price: {price}</h2>
+              <button className="px-2 rounded-md border-2 border-gray-300  ">Choose Player</button>
+            </div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Player;
--- Projects/dream-11/src/components/Players/Players.jsx
+++ Projects/dream-11/src/components/Players/Players.jsx
@@ -1,42 +1,46 @@
 import { useEffect, useState } from "react";
 import Player from "../Player/Player";
 
-
 const Players = () => {
-    const [players, setPlayer] = useState([]);
-    useEffect(()=>{
-        fetch('players.json')
-        .then((res=>res.json()))
-        .then(data=>setPlayer(data))
-    },[])
-    const handlePlayers=(player)=>{
-        // console.log(player)
-        const newPlayer = [...players, player];
-        setPlayer(newPlayer)
-    }
-    return (
-        <div className="w-10/12 mx-auto pt-10">
-            <div className="flex justify-between items-center">
-                <h2 className="text-black text-2xl font-bold">Available Players</h2>
-                <div className="flex flex-row gap-2 border-2 p-2 rounded-md">
-                <button onClick={()=>handlePlayers(players)} className="border-2 p-1 rounded-md">Available</button><button>Selected</button>
-                </div>
-            </div>
-            {/* <Player></Player> */}
-            
-            {
-               
-               players.map((play,indx)=> (
-                    <Player
-                    key={indx}
-                        player={play}
-                    >
-
-                    </Player>
-               ))
-            }
+  const [players, setPlayer] = useState([]);
+  useEffect(() => {
+    fetch("players.json")
+      .then((res) => res.json())
+      .then((data) => setPlayer(data));
+  }, []);
+//   const handlePlayers = (player) => {
+//     // console.log(player)
+//     const newPlayer = [...players, player];
+//     setPlayer(newPlayer);
+//   };
+  return (
+    <div className="w-10/12 mx-auto pt-10">
+      <div className="flex justify-between items-center">
+        <h2 className="text-black text-2xl font-bold">Available Players</h2>
+        <div className="flex flex-row gap-2 border-2 p-2 rounded-md">
+          <button
+            // onClick={() => handlePlayers(players)}
+            className="border-2 p-1 rounded-md"
+          >
+            Available
+          </button>
+          <button>Selected</button>
         </div>
-    );
+      </div>
+      {/* <Player></Player> */}
+
+      {
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3  gap-6 mt-6">
+          {players.map((play, indx) => (
+            <Player key={indx} player={play}></Player>
+          ))}
+        </div>
+      }
+    </div>
+  );
+
+  
+  
 };
 
 export default Players;
